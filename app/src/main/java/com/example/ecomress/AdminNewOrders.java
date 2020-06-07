fba60e598ce30113cb7481bdbe83559498b6c081
ahmad <you@example.com>
2020-06-07 21:41:24 +0500

    Ecomress project Intent

--- app/src/main/java/com/example/ecomress/AdminNewOrders.java
+++ app/src/main/java/com/example/ecomress/AdminNewOrders.java
@@ -3,12 +3,17 @@ package com.example.ecomress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AdminNewOrders extends AppCompatActivity {
+    private String CatagoryName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_new_orders);
+        CatagoryName=getIntent().getExtras().get("Category").toString();
+        Toast.makeText(this,CatagoryName ,Toast.LENGTH_SHORT).show();
     }
 }
